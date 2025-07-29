280445064b7c3df78dd12377e1a34be6c8f0cbb2
wuhuiquan <wuhuiquan4@huawei.com>
2025-07-29 09:47:43 +0800

    [clangd] fix invalid source location

--- clang/lib/Parse/ParseStmt.cpp
+++ clang/lib/Parse/ParseStmt.cpp
@@ -1395,8 +1395,8 @@ StmtResult Parser::ParseCompoundStatementBody(bool isStmtExpr
           }
           if (Decls.size()) {
             DeclGroupPtrTy DataPT = Actions.BuildDeclaratorGroup(Decls);
-            StmtResult SR = Actions.ActOnDeclStmt(DataPT, SourceLocation(),
-                                                  SourceLocation());
+            StmtResult SR = Actions.ActOnDeclStmt(DataPT, DS->getBeginLoc(),
+                                                  DS->getEndLoc());
             R = SR;
           }
         }
