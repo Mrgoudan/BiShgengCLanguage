04bfc5f72704baf0f31d2e0a41ae0813dd5ef4a2
wangxizao <wangxizao@huawei.com>
2026-03-31 11:41:23 +0800

    [diag] fix missing warning option names

--- clang/lib/Frontend/TextDiagnosticPrinter.cpp
+++ clang/lib/Frontend/TextDiagnosticPrinter.cpp
@@ -78,18 +78,12 @@ static void printDiagnosticOptions(raw_ostream &OS,
 
     StringRef Opt = DiagnosticIDs::getWarningOptionForDiag(Info.getID());
     if (!Opt.empty()) {
-#if ENABLE_BSC
-      if (!DiagnosticIDs::isDefaultMappingAsError(Info.getID())) {
-#endif
         OS << (Started ? "," : " [")
            << (Level == DiagnosticsEngine::Remark ? "-R" : "-W") << Opt;
         StringRef OptValue = Info.getDiags()->getFlagValue();
         if (!OptValue.empty())
           OS << "=" << OptValue;
         Started = true;
-#if ENABLE_BSC
-      }
-#endif
     }
   }
 
